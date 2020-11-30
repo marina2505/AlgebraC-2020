10afe55814a9e30d08417d62252e10fd813a670a
marina.pour1 <marina.pour1@gmail.com>
2020-11-30 16:09:40 +0100

    domaća zadaća

--- 01_domaci_zadatak/Program.cs
+++ 01_domaci_zadatak/Program.cs
@@ -12,14 +12,26 @@ namespace _01_domaci_zadatak
             Console.WriteLine(1);
             Console.WriteLine(2);
             Console.WriteLine(3);
+            Console.ReadLine();
 
             //4.1.3 - Ledište/Vrelište
             //Napišite program koji će ispisivati temperaturu ledišta i vrelišta
             //vode, svaku u novi red
 
+       
+        
+            int lediste = 0;
+            int vreliste = 100;
+            Console.WriteLine("Ledište vode je na {0} stupnjeva Celzijusa", lediste);
+            Console.WriteLine("Vrelište vode je na {100} stupenjva Celzijusa", vreliste);
+
+            _ = System.Console.ReadKey();
+           
+
             //5.1.1 - Zbroj i razlika
             //Program koji traži unos 2 cijela broja i prikazuje
             //na ekranu njihov zbroj te razliku
+
 
             //5.1.2 - Umnožak i kvocijent
             //Program koji traži unos 2 cijela broja i prikazuje 
